9637b52d3f1f8fc4ac401b92b2a4478a92bc8f23
teja679 <lovlytejaa@gmail.com>
2022-11-03 19:08:05 +0530

    login & singup aded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+/*
+    <div className="App">
+      <Login />
+     <Register />
+    </div>*/
